95657da7a1359b85a5a90ff9ef1145cfed961438
tacobell1988 <34141697+tacobell1988@users.noreply.github.com>
2023-06-17 01:02:55 -0500

    Update gcloudignore.sh

--- gcloudignore.sh
+++ gcloudignore.sh
@@ -6,7 +6,8 @@ regex=""
 while IFS= read -r line; do
   # Ignore comments and empty lines
   if [[ "$line" =~ ^[^#]*[^[:space:]]$ ]]; then
-    # Escape the dot character
+    # Escape the dot character and add ^ and $
+    line="^$line$"
     line=${line//./\\.}
     regex+="|$line"
   fi
@@ -17,5 +18,6 @@ regex=${regex#|}
 
 # Export the generated regular expression as an environment variable
 export GSUTIL_IGNORE="$regex"
+
 
 echo $GSUTIL_IGNORE
